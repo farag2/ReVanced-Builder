be97aa94dde6fa112d83bc086e9a397713dd55f3
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-23 10:33:06 +0300

    Update Zulu_JDK.ps1

--- Scripts/Zulu_JDK.ps1
+++ Scripts/Zulu_JDK.ps1
@@ -8,10 +8,22 @@ $URL = (Invoke-RestMethod @Parameters).architecture."64bit".url
 $ZuluTag = (Invoke-RestMethod @Parameters).version
 $Parameters = @{
     Uri             = $URL
-    Outfile         = "$PSScriptRoot\jdk_windows-x64_bin.zip"
+    Outfile         = "Temp\jdk_windows-x64_bin.zip"
     UseBasicParsing = $true
     Verbose         = $true
 }
 Invoke-RestMethod @Parameters
 
 echo "ZuluTag=$ZuluTag" >> $env:GITHUB_ENV
+
+Write-Verbose -Message Expanding Zulu JDK -Verbose
+
+$Parameters = @{
+    Path            = "Temp\jdk_windows-x64_bin.zip"
+    DestinationPath = "Temp\jdk_windows-x64_bin"
+    Force           = $true
+    Verbose         = $true
+}
+Expand-Archive @Parameters
+
+Remove-Item -Path "Temp\jdk_windows-x64_bin.zip" -Force
