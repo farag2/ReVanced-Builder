9ad366de32a15f6d9a51e5fed86826a103581f3d
Dmitry Nefedov <nefedovds@ya.ru>
2023-02-06 22:35:33 +0300

    Update ReVanced_CLI.ps1

--- Scripts/ReVanced_CLI.ps1
+++ Scripts/ReVanced_CLI.ps1
@@ -1,16 +1,23 @@
 # https://github.com/revanced/revanced-cli
+$Token = "${{ secrets.GITHUB_TOKEN }}"
+$Headers = @{
+	Accept        = "application/json"
+	Authorization = "Bearer $Token"
+}
 $Parameters = @{
-    Uri             = "https://api.github.com/repos/revanced/revanced-cli/releases/latest"
-    UseBasicParsing = $true
-    Verbose         = $true
+	Uri             = "https://api.github.com/repos/revanced/revanced-cli/releases/latest"
+	Headers         = $Headers
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 $CLIvtag = (Invoke-RestMethod @Parameters).tag_name
 $CLItag = $CLIvtag.replace("v", "")
 $Parameters = @{
-    Uri             = "https://github.com/revanced/revanced-cli/releases/download/$CLIvtag/revanced-cli-$CLItag-all.jar"
-    Outfile         = "Temp\revanced-cli.jar"
-    UseBasicParsing = $true
-    Verbose         = $true
+	Uri             = "https://github.com/revanced/revanced-cli/releases/download/$CLIvtag/revanced-cli-$CLItag-all.jar"
+	Outfile         = "Temp\revanced-cli.jar"
+	Headers         = $Headers
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-RestMethod @Parameters
 
