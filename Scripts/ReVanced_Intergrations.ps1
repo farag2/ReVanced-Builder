95eadecda24a0459f41c6a8a0bf8577b4631bf34
Dmitry Nefedov <nefedovds@ya.ru>
2023-02-06 19:09:55 +0300

    Update ReVanced_Intergrations.ps1

--- Scripts/ReVanced_Intergrations.ps1
+++ Scripts/ReVanced_Intergrations.ps1
@@ -6,7 +6,7 @@ $Parameters = @{
 }
 $IntegrationsTag = (Invoke-RestMethod @Parameters).tag_name
 $Parameters = @{
-    Uri             = "https://github.com/revanced/revanced-integrations/releases/download/$IntegrationsTag/app-release-unsigned.apk"
+    Uri             = "https://github.com/revanced/revanced-integrations/releases/download/$IntegrationsTag/revanced-integrations-$IntegrationsTag.apk"
     Outfile         = "Temp\app-release-unsigned.apk"
     UseBasicParsing = $true
     Verbose         = $true
