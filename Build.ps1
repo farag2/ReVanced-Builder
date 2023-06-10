a33f2afa35109165f938683d8638ffd6254b5ffc
Dmitry Nefedov <nefedovds@ya.ru>
2023-06-10 21:36:40 +0300

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -44,7 +44,7 @@ $LatestSupported = $LatestSupported.replace(".", "-")
 # Get unique key to generate direct link
 # https://www.apkmirror.com/apk/google-inc/youtube/
 $Parameters = @{
-	Uri             = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-2-android-apk-download/"
+	Uri             = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-android-apk-download/"
 	UseBasicParsing = $false # Disabled
 	Verbose         = $true
 }
@@ -52,7 +52,7 @@ $Request = Invoke-Webrequest @Parameters
 $nameProp = $Request.ParsedHtml.getElementsByClassName("accent_bg btn btn-flat downloadButton") | ForEach-Object -Process {$_.nameProp}
 
 $Parameters = @{
-	Uri             = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-2-android-apk-download/download/$($nameProp)"
+	Uri             = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-android-apk-download/download/$($nameProp)"
 	UseBasicParsing = $false # Disabled
 	Verbose         = $true
 }
