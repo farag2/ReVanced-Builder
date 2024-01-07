6004e821a769f090d0eb45736b110ad0c2cf0048
Dmitry Nefedov <nefedovds@ya.ru>
2024-01-07 15:40:16 +0300

    Update YouTube.ps1

--- Scripts/YouTube.ps1
+++ Scripts/YouTube.ps1
@@ -65,4 +65,4 @@ $Parameters = @{
 }
 Invoke-Webrequest @Parameters
 
-echo "LatestSupportedYT=$LatestSupportedYT.replace('-', '.')" >> $env:GITHUB_ENV
+echo "LatestSupportedYT=$($LatestSupportedYT.replace('-', '.'))" >> $env:GITHUB_ENV
