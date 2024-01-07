2d21ea113c39c99c5155eee2bf9785fca5c7eece
Dmitry Nefedov <nefedovds@ya.ru>
2024-01-07 15:37:57 +0300

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -136,7 +136,7 @@ $Parameters = @{
 }
 Invoke-RestMethod @Parameters
 
-# https://github.com/TeamVanced/VancedMicroG
+# https://github.com/inotia00/VancedMicroG
 $Parameters = @{
 	Uri             = "https://api.github.com/repos/inotia00/VancedMicroG/releases/latest"
 	UseBasicParsing = $true
