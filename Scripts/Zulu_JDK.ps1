2bce9d96ea5961d6cf618380d7ac24a4bfb7e866
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-17 17:05:32 +0300

    Update Zulu_JDK.ps1

--- Scripts/Zulu_JDK.ps1
+++ Scripts/Zulu_JDK.ps1
@@ -2,7 +2,7 @@
 
 # https://app.swaggerhub.com/apis-docs/azul/zulu-download-community/1.0
 $Parameters = @{
-	Uri             = "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=&bundle_type=jdk&javafx=false&ext=msi&os=windows&arch=x86&hw_bitness=64"
+	Uri             = "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=&bundle_type=jdk&javafx=false&ext=zip&os=windows&arch=x86&hw_bitness=64"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
