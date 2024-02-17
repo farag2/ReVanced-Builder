6b5ac13ca482a7aa25a9ab2008419003938fa3fc
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-17 14:05:45 +0300

    Update YouTube.ps1

--- Scripts/YouTube.ps1
+++ Scripts/YouTube.ps1
@@ -10,55 +10,17 @@ $versions = ($JSON | Where-Object -FilterScript {$_.compatiblePackages.name -eq 
 $LatestSupported = $versions | Sort-Object -Descending -Unique | Select-Object -First 1
 $LatestSupportedYT = $LatestSupported.replace(".", "-")
 
-$AngleSharpAssemblyPath = (Get-ChildItem -Path (Split-Path -Path (Get-Package -Name AngleSharp).Source) -Filter "*.dll" -Recurse | Where-Object -FilterScript {$_ -match "standard"} | Select-Object -Last 1).FullName
-Add-Type -Path $AngleSharpAssemblyPath
-
 # We need a NON-bundle version
-# We check whether output exists. The link that has the output is what we need then
-# https://www.apkmirror.com/apk/google-inc/youtube/
+# https://apkpure.net/ru/youtube/com.google.android.youtube/versions
 $Parameters = @{
-	Uri             = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-android-apk-download/"
-	UseBasicParsing = $false # Disabled
-	Verbose         = $true
-}
-$UriParse = (Invoke-Webrequest @Parameters).Links | Where-Object -FilterScript {($_.outerHTML -match "Download APK") -and ($_.outerHTML -notmatch "Bundle")}
-
-# Check if variable contains a data
-if ($UriParse)
-{
-	$Request = Invoke-Webrequest @Parameters
-	$Uri     = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-android-apk-download/"
-}
-
-$Parameters = @{
-	Uri             = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-2-android-apk-download/"
-	UseBasicParsing = $false # Disabled
-	Verbose         = $true
-}
-$UriParse = (Invoke-Webrequest @Parameters).Links | Where-Object -FilterScript {($_.outerHTML -match "Download APK") -and ($_.outerHTML -notmatch "Bundle")}
-
-# Check if variable contains a data
-if ($UriParse)
-{
-	$Request = Invoke-Webrequest @Parameters
-	$Uri     = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$($LatestSupported)-release/youtube-$($LatestSupported)-2-android-apk-download/"
-}
-
-$Parsed = (New-Object -TypeName AngleSharp.Html.Parser.HtmlParser).ParseDocument($Request.Content)
-$Key = $Parsed.All | Where-Object -FilterScript {$_.ClassName -match "accent_bg btn btn-flat downloadButton"} | ForEach-Object -Process {$_.Search}
-
-$Parameters = @{
-	Uri             = "$($Uri)/download/$($Key)"
+	Uri             = "https://apkpure.net/ru/youtube/com.google.android.youtube/download/$($LatestSupported)"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
-$Request = Invoke-Webrequest @Parameters
-$Parsed = (New-Object -TypeName AngleSharp.Html.Parser.HtmlParser).ParseDocument($Request.Content)
-$Key = ($Parsed.All | Where-Object -FilterScript {$_.InnerHtml -eq "here"}).Search
+$DownloadURL = (Invoke-Webrequest @Parameters).Links.href | Where-Object -FilterScript {$_ -match "APK/com.google.android.youtube"} | Select-Object -Index 1
 
-# Get the real link
 $Parameters = @{
-	Uri             = "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php$Key"
+	Uri             = $DownloadURL
 	OutFile         = "Temp\youtube.apk"
 	UseBasicParsing = $true
 	Verbose         = $true
