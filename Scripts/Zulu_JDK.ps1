3ec6f2b0d296c089143872e9829d182d5d1a6108
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-17 17:27:35 +0300

    Update Zulu_JDK.ps1

--- Scripts/Zulu_JDK.ps1
+++ Scripts/Zulu_JDK.ps1
@@ -2,7 +2,7 @@
 
 # https://app.swaggerhub.com/apis-docs/azul/zulu-download-community/1.0
 $Parameters = @{
-	Uri             = "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=&bundle_type=jdk&javafx=false&ext=zip&os=windows&arch=x86&hw_bitness=64"
+	Uri             = "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=&bundle_type=jdk&javafx=false&ext=msi&os=windows&arch=x86&hw_bitness=64"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
@@ -11,22 +11,22 @@ $URL = (Invoke-RestMethod @Parameters).url
 $ZuluTag = [string](Invoke-RestMethod @Parameters).jdk_version -replace (" ", ".")
 echo "ZuluTag=$ZuluTag" >> $env:GITHUB_ENV
 
-# Save zulu-jdk-win_x64.msi as zulu-jdk-win_x64.zip with purpose
 $Parameters = @{
-	Uri             = $URL
-	Outfile         = "Temp\zulu-jdk-win_x64.zip"
-	UseBasicParsing = $true
-	Verbose         = $true
+    Uri             = $URL
+    Outfile         = "Temp\zulu-jdk-win_x64.msi"
+    UseBasicParsing = $true
+    Verbose         = $true
 }
 Invoke-RestMethod @Parameters
 
-Write-Verbose -Message "Expanding Zulu JDK" -Verbose
+Write-Verbose -Message "Installing Zulu JDK" -Verbose
 
-# Expand jdk_windows-x64_bin archive
-$Parameters = @{
-	Path            = "Temp\zulu-jdk-win_x64.zip"
-	DestinationPath = "Temp\zulu-jdk-win_x64"
-	Force           = $true
-	Verbose         = $true
-}
-Expand-Archive @Parameters
+$Arguments = @(
+	"/i `"Temp\zulu-jdk-win_x64.msi`"",
+	"/quiet",
+	"/qb",
+	"/norestart"
+)
+Start-Process -FilePath "msiexec" -ArgumentList $Arguments -Wait
+
+Remove-Item -Path "Temp\zulu-jdk-win_x64.msi" -Force
