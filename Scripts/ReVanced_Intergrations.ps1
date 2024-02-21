6e8718da91d27261a0084423a507e9abec8841f1
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-21 23:34:02 +0300

    Update ReVanced_Intergrations.ps1

--- Scripts/ReVanced_Intergrations.ps1
+++ Scripts/ReVanced_Intergrations.ps1
@@ -6,9 +6,8 @@ $Parameters = @{
 }
 $IntegrationsTag = (Invoke-RestMethod @Parameters).tag_name
 
-# https://github.com/revanced/revanced-integrations/releases/download/$IntegrationsTag/revanced-integrations-$($IntegrationsTag.replace(`"v`", `"`")).apk
 $Parameters = @{
-    Uri             = "https://github.com/ReVanced/revanced-integrations/releases/download/v1.4.0-dev.4/revanced-integrations-1.4.0-dev.4.apk"
+    Uri             = "https://github.com/revanced/revanced-integrations/releases/download/$IntegrationsTag/revanced-integrations-$($IntegrationsTag.replace(`"v`", `"`")).apk"
     Outfile         = "ReVanced\revanced-integrations.apk"
     UseBasicParsing = $true
     Verbose         = $true
