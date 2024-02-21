25d603521677af25da3dde09e840183034b6f492
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-21 23:04:06 +0300

    Update ReVanced_Patches.ps1

--- Scripts/ReVanced_Patches.ps1
+++ Scripts/ReVanced_Patches.ps1
@@ -6,8 +6,10 @@ $Parameters = @{
 }
 $Patchesvtag = (Invoke-RestMethod @Parameters).tag_name
 $Patchestag = $Patchesvtag.replace("v", "")
+
+# "https://github.com/revanced/revanced-patches/releases/download/$Patchesvtag/revanced-patches-$Patchestag.jar"
 $Parameters = @{
-    Uri             = "https://github.com/revanced/revanced-patches/releases/download/$Patchesvtag/revanced-patches-$Patchestag.jar"
+    Uri             = "https://github.com/ReVanced/revanced-patches/releases/download/v4.3.0-dev.3/revanced-patches-4.3.0-dev.3.jarr"
     Outfile         = "ReVanced\revanced-patches.jar"
     UseBasicParsing = $true
     Verbose         = $true
