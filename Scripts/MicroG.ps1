1c05a501d1ebf32658d043139db655c0816867b1
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-25 22:36:25 +0300

    Update MicroG.ps1

--- Scripts/MicroG.ps1
+++ Scripts/MicroG.ps1
@@ -1,16 +1,16 @@
 # https://github.com/inotia00/VancedMicroG
 $Parameters = @{
-    Uri             = "https://api.github.com/repos/inotia00/VancedMicroG/releases/latest"
-    UseBasicParsing = $true
-    Verbose         = $true
+	Uri             = "https://api.github.com/repos/inotia00/VancedMicroG/releases/latest"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 $MicroGTag = (Invoke-RestMethod @Parameters).tag_name
 $Parameters = @{
-     Uri             = "https://github.com/inotia00/VancedMicroG/releases/download/$MicroGTag/microg.apk"
-     Outfile         = "ReVanced\microg.apk"
-     UseBasicParsing = $true
-     Verbose         = $true
+	Uri             = "https://github.com/inotia00/VancedMicroG/releases/download/$MicroGTag/microg.apk"
+	Outfile         = "ReVanced\microg.apk"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
-Invoke-RestMethod @Parameters
+Invoke-RestMethod @Parameter
 
 echo "MicroGTag=$MicroGTag" >> $env:GITHUB_ENV
