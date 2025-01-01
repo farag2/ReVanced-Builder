28360d89f6867bef3e82b332c51155fe9a85e1a5
Dmitry Nefedov <nefedovds@ya.ru>
2025-01-01 20:52:37 +0300

    Update MicroG.ps1

--- Scripts/MicroG.ps1
+++ Scripts/MicroG.ps1
@@ -1,26 +1,26 @@
 # https://github.com/ReVanced/GmsCore
-$Parameters = @{
+$URLParameters = @{
 	Uri             = "https://api.github.com/repos/ReVanced/GmsCore/releases/latest"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
 # Default apk
-$URL = (Invoke-RestMethod @Parameters).assets.browser_download_url | Where-Object -FilterScript {$_ -notmatch "hw"}
-$MicroGTag = (Invoke-RestMethod @Parameters).tag_name
+$URL = (Invoke-RestMethod @URLParameters).assets.browser_download_url | Where-Object -FilterScript {$_ -notmatch "hw"}
+$MicroGTag = (Invoke-RestMethod @URLParameters).tag_name
 
 $Parameters = @{
 	Uri             = $URL
-	Outfile         = "ReVanced_Builder\microg.apk"
+	Outfile         = "D:\Downloads\Cien.Anos.de.Soledad.S01.1080p.WEB-DL.4xRus.Spa-alekartem\microg.apk"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
 Invoke-RestMethod @Parameters
 
 # Huawei apk
-$URL = (Invoke-RestMethod @Parameters).assets.browser_download_url | Where-Object -FilterScript {$_ -match "hw"}
+$URL = (Invoke-RestMethod @URLParameters).assets.browser_download_url | Where-Object -FilterScript {$_ -match "hw"}
 $Parameters = @{
 	Uri             = $URL
-	Outfile         = "ReVanced_Builder\microg-huawei.apk"
+	Outfile         = "D:\Downloads\Cien.Anos.de.Soledad.S01.1080p.WEB-DL.4xRus.Spa-alekartem\microg-huawei.apk"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
